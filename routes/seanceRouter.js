408735fd9b7d34c1d62682d8a9ff865260797d6f
Daemio <masterrmint@gmail.com>
2018-10-26 09:47:37 +0300

    fixed seance router

--- routes/seanceRouter.js
+++ routes/seanceRouter.js
@@ -1,31 +1,78 @@
 const Router = require('koa-router');
-const {appointSeance, cancelSeance} = require('../utils/dbUtils');
+const {appointSeance, cancelSeance, getSeance} = require('../utils/dbUtils');
 
 const seanceRouter = new Router();
 
-seanceRouter.post('/appoint_seance', async (ctx) => { //по-хорошему id юзера нужно брать из токена
-    //предварительно пройдя middleware аутентификации
-    const seanceParams = {user_id, seance_id} = ctx.request.body;
-    try {
-        await appointSeance(seanceParams); //добавить валидацию параметров(минимум наличие), проверку занятости сеанса, но это завтра 
-        ctx.status = 200;
-        ctx.body = {message: "ok"}
-    } catch (err) {
-        ctx.status = 500;
-        ctx.body = {message:"Could not appoint seance", error: err}
-    }
-});
+seanceRouter.post('/appoint_seance',
+    paramsValidationMiddleware, 
+    async (ctx) => { //по-хорошему id юзера нужно брать из токена
+        //предварительно пройдя middleware аутентификации и вообще как минимум посты должны быть защищенными
+        try {
+            const seance = await getSeance(ctx.seance_id);
 
-seanceRouter.post('/cancel_seance', async (ctx) => { //тут это еще важней, так как мы можем отменять чужые сеансы
-    const seanceParams = {user_id, seance_id} = ctx.request.body;
-    try {
-        await cancelSeance(seance_id);
-        ctx.status = 200;
-        ctx.body = {message: "ok"}
-    } catch (err) {
-        ctx.status = 500;
-        ctx.body = {message:"Could not cancel seance", error: err}
-    }
-});
+            if(!seance.available) {
+                ctx.status = 403;
+                ctx.body = {
+                    message: "Error: Could not appoint seance",
+                    error: "Seance slot is busy"
+                };
+                return;
+            }
 
+            await appointSeance({
+                seance_id: ctx.seance_id,
+                user_id: ctx.user_id
+            });
+
+            ctx.status = 200;
+            ctx.body = {message: "Successfully appointed seance", seance_id: seance._id}
+        } catch (error) {
+            ctx.status = 500;
+            ctx.body = {message: "Error: Could not appoint seance", error}
+        }
+    });
+
+seanceRouter.post('/cancel_seance', 
+    paramsValidationMiddleware,
+    async (ctx) => { //тут это еще важней, так как мы можем отменять чужые сеансы, зная _id
+        try {
+            const seance = await getSeance(ctx.seance_id);
+
+            if(ctx.user_id != seance.user_id) { //так хотя бы нельзя будет отменить чужой сеанс, не зная чужой id, но опять же
+                ctx.status = 403;
+                ctx.body = {
+                    message: "Error: Could not cancel seance", 
+                    error: "You can only cancel seances you appointed"
+                };
+                return;
+            }
+
+            await cancelSeance(ctx.seance_id);
+            ctx.status = 200;
+            ctx.body = {message: "Successfully canceled seance", seance_id: seance._id}
+        } catch (error) {
+            ctx.status = 500;
+            ctx.body = {message: "Could not cancel seance", error}
+        }
+    });
+
+function paramsValidationMiddleware(ctx, next) {
+    const {user_id, seance_id} = ctx.request.body;
+    const missing = [];
+    if(!user_id) {
+        missing.push('{user_id}');
+    }
+    if(!seance_id) {
+        missing.push('{seance_id}');
+    }
+    if(missing.length > 1){
+        ctx.status = 400;
+        ctx.body = {message: `Missing required parameter(s) ${missing}`};
+        return;
+    } else {
+        ctx.user_id = user_id;
+        ctx.seance_id = seance_id;
+        return next(ctx);
+    }
+}
 module.exports = seanceRouter;
